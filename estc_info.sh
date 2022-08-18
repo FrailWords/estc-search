2f3790bb84191eaf937a28a79b5fbadc954f3f95
Sriram Viswanathan <sriram.vish@gmail.com>
2022-08-18 21:19:02 +0530

    Add poetry to path if it doens't exist

--- estc_info.sh
+++ estc_info.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+function addToPATH {
+  case ":$PATH:" in
+    *":$1:"*) :;; # already there
+    *) PATH="$1:$PATH";; # or PATH="$PATH:$1"
+  esac
+}
+
 # Add poetry to path if its not there already
-export -U PATH=$HOME/.poetry/bin${PATH:+:$PATH}
+addToPATH $HOME/.poetry/bin
 
 # run parser
 poetry run chewfiles --estc_number $1
